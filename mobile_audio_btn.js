c3b3c9a2394923229f29e5dcb1f9b3848af3292e
AristAlfredCPT2019 <51200085+AristAlfredCPT2019@users.noreply.github.com>
2019-06-01 12:06:43 -0400

    Added a JS file for playing final.html audio (mobile users)

---
+++ mobile_audio_btn.js
@@ -0,0 +1,8 @@
+//create a function for a button that, when clicked, plays sound (specifically for mobile browser restrictions)
+function mobileCheerPlay(){
+	//initialize a variable to the cheering audio
+	var cheer_audio = document.getElementById('crowd_cheering')
+
+	//play the audio
+	cheer_audio.play();
+}
