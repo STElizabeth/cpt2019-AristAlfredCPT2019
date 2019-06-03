45f6d300293c49b8f4e3710e5f2ea0141013600d
AristAlfredCPT2019 <51200085+AristAlfredCPT2019@users.noreply.github.com>
2019-06-03 08:56:08 -0400

    Removed mobile "sound button" script

--- mobile_audio_btn.js
+++
@@ -1,8 +0,0 @@
-//create a function for a button that, when clicked, plays sound (specifically for mobile browser restrictions)
-function mobileCheerPlay(){
-	//initialize a variable to the cheering audio
-	var cheer_audio = document.getElementById('crowd_cheering')
-
-	//play the audio
-	cheer_audio.play();
-}
